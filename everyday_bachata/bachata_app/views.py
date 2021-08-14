0b637c8df8d0429097b7d01d97fd213d74d7d3ee
Barbara Rafał <barbara.rafal@gmail.com>
2021-08-14 15:03:57 +0200

    views.py changed direction to html file

--- everyday_bachata/bachata_app/views.py
+++ everyday_bachata/bachata_app/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 def bachata(request):
     return render(
-        request, 'bachata.html'
+        request, 'bachata_app/bachata.html'
     )
