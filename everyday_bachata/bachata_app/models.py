e9f3e89e954c032f7a8c4224571b7051fe21c05a
Barbara Rafał <barbara.rafal@gmail.com>
2021-08-17 19:53:26 +0200

    changes in User model

--- everyday_bachata/bachata_app/models.py
+++ everyday_bachata/bachata_app/models.py
@@ -1,20 +1,21 @@
 from django.db import models
 
 # Create your models here.
-
-class User(models.Model):
-     login = models.CharField(max_length=50)
-     password = models.CharField(max_length=8)
-     email= models.CharField(max_length=50)
-
-     def __str__(self):
-         return f"{self.login} , {self.email}"
-
 class UserType(models.Model):
     user_type = models.CharField(max_length=10)
 
     def __str__(self):
         return f"{self.user_type}"
+
+
+class User(models.Model):
+     login = models.CharField(max_length=50)
+     password = models.CharField(max_length=8)
+     email= models.CharField(max_length=50)
+     type = models.OneToOneField(UserType, on_delete=models.CASCADE)
+
+     def __str__(self):
+         return f"{self.login} , {self.email}"
 
 
 class Organizer(models.Model):
